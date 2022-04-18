35af7b4918dd211323595230fe89af355d27a865
hritik <86347286+hritikd3@users.noreply.github.com>
2022-04-18 10:16:47 +0530

    function added

--- src/Components/Doctors.js
+++ src/Components/Doctors.js
@@ -12,19 +12,19 @@ function Doctors() {
         <div className='box-container'>
           <div className='box'>
             <img src='../image/doc-1.jpg' alt='' />
-            <h3>john deo</h3>
+            <h3>Dr. Sneha Upadhyay</h3>
             <span>expert doctor</span>
             <div className='share'>
-              <a href='#' className='fab fa-facebook-f'>
+              <a href='#' className='fab fa-facebook-f' onClick={()=> alert('we are collaborating currently with mental health experts and soon onboard them on out portal :)>
                 {' '}
               </a>
-              <a href='#' className='fab fa-twitter'>
+              <a href='#' className='fab fa-twitter' onClick={()=> alert('we are collaborating currently with mental health experts and soon onboard them on out portal :)>
                 {' '}
               </a>
-              <a href='#' className='fab fa-instagram'>
+              <a href='#' className='fab fa-instagram' onClick={()=> alert('we are collaborating currently with mental health experts and soon onboard them on out portal :)>
                 {' '}
               </a>
-              <a href='#' className='fab fa-linkedin'>
+              <a href='#' className='fab fa-linkedin ' onClick={()=> alert('we are collaborating currently with mental health experts and soon onboard them on out portal :)>
                 {' '}
               </a>
             </div>
@@ -32,31 +32,31 @@ function Doctors() {
 
           <div className='box'>
             <img src='../image/doc-2.jpg' alt='' />
-            <h3>john deo</h3>
+            <h3>Dr. Vishesh Bandi</h3>
             <span>expert doctor</span>
             <div className='share'>
-              <a href='#' className='fab fa-facebook-f'></a>
+              <a href='#' className='fab fa-facebook-f' onClick={()=> alert('we are collaborating currently with mental health experts and soon onboard them on out portal :)></a>
               <a href='#' className='fab fa-twitter'></a>
-              <a href='#' className='fab fa-instagram'></a>
+              <a href='#' className='fab fa-instagram'onClick={()=> alert('we are collaborating currently with mental health experts and soon onboard them on out portal :)></a>
               <a href='#' className='fab fa-linkedin'></a>
             </div>
           </div>
 
           <div className='box'>
             <img src='../image/doc-3.jpg' alt='' />
-            <h3>john deo</h3>
+            <h3>Rahul Sharma</h3>
             <span>expert doctor</span>
             <div className='share'>
-              <a href='# ' className='fab fa-facebook-f'>
+              <a href='# ' className='fab fa-facebook-f' onClick={()=> alert('we are collaborating currently with mental health experts and soon onboard them on out portal :)>
                 {' '}
               </a>
-              <a href='# ' className='fab fa-twitter'>
+              <a href='# ' className='fab fa-twitter' onClick={()=> alert('we are collaborating currently with mental health experts and soon onboard them on out portal :)>
                 {' '}
               </a>
-              <a href='# ' className='fab fa-instagram'>
+              <a href='# ' className='fab fa-instagram' onClick={()=> alert('we are collaborating currently with mental health experts and soon onboard them on out portal :)>
                 {' '}
               </a>
-              <a href=' # ' className='fab fa-linkedin'>
+              <a href=' # ' className='fab fa-linkedin' onClick={()=> alert('we are collaborating currently with mental health experts and soon onboard them on out portal :)>
                 {' '}
               </a>
             </div>
